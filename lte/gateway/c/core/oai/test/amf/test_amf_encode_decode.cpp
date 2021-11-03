b85e22ae805815f8db5979b949febf2113307e72
shashidhar-patil <shashidhar.patil@wavelabs.ai>
2021-11-03 17:53:30 +0530

    fix(mme): Changing the hashtable implementations for UE mgmt in amf to maps
    Signed-off-by: shashidhar-patil <shashidhar.patil@wavelabs.ai>

--- lte/gateway/c/core/oai/test/amf/test_amf_encode_decode.cpp
+++ lte/gateway/c/core/oai/test/amf/test_amf_encode_decode.cpp
@@ -16,6 +16,8 @@
 #include "../../tasks/amf/amf_app_ue_context_and_proc.h"
 #include "lte/gateway/c/core/oai/include/mme_config.h"
 #include "lte/gateway/c/core/oai/tasks/amf/amf_authentication.h"
+#include "lte/gateway/c/core/oai/tasks/amf/amf_map.h"
+#include "lte/gateway/c/core/oai/tasks/amf/amf_app_defs.h"
 
 extern "C" {
 #include "lte/gateway/c/core/oai/common/dynamic_memory_check.h"
@@ -730,6 +732,87 @@ TEST(test_delete_registration_proc, test_delete_registration_proc) {
 
   delete_wrapper(&ue_ctxt->amf_context.amf_procedures);
   delete ue_ctxt;
+}
+
+TEST(test_map, test_map) {
+  // Initializations for Map: Key-uint64_t Data-uint64_t
+  uint64_t data;
+  amf_app_desc_t* state_cache_p = new (amf_app_desc_t);
+
+  // Initializations for Map: Key-guti_m5_t Data-uint64_t
+  guti_m5_t guti_1;
+  guti_1.m_tmsi = 0x2bfb815f;
+  guti_m5_t guti_2;
+  guti_2.m_tmsi = 0x1afc831f;
+  uint64_t gutiData;
+
+  state_cache_p->amf_ue_contexts.imsi_amf_ue_id_htbl.set_name("IMSI HASHTABLE");
+  EXPECT_EQ(
+      state_cache_p->amf_ue_contexts.imsi_amf_ue_id_htbl.get_name(),
+      "IMSI HASHTABLE");
+
+  // Trying to get from an empty map
+  EXPECT_EQ(
+      state_cache_p->amf_ue_contexts.imsi_amf_ue_id_htbl.get(2, &data),
+      MAP_EMPTY);
+
+  // Inserting new <key,value> pair
+  EXPECT_EQ(
+      state_cache_p->amf_ue_contexts.imsi_amf_ue_id_htbl.insert(1, 10), MAP_OK);
+  EXPECT_EQ(
+      state_cache_p->amf_ue_contexts.imsi_amf_ue_id_htbl.insert(2, 20), MAP_OK);
+
+  // Inserting already existing key.Expected: failure.
+  EXPECT_EQ(
+      state_cache_p->amf_ue_contexts.imsi_amf_ue_id_htbl.insert(1, 20),
+      MAP_KEY_ALREADY_EXISTS);
+
+  // //Getting data from map
+  EXPECT_EQ(
+      state_cache_p->amf_ue_contexts.imsi_amf_ue_id_htbl.get(1, &data), MAP_OK);
+  EXPECT_EQ(data, 10);
+
+  // Getting with invalid key
+  EXPECT_EQ(
+      state_cache_p->amf_ue_contexts.imsi_amf_ue_id_htbl.get(5, &data),
+      MAP_KEY_NOT_EXISTS);
+
+  // Removing entry from table
+  EXPECT_EQ(
+      state_cache_p->amf_ue_contexts.imsi_amf_ue_id_htbl.remove(1), MAP_OK);
+
+  // Trying to remove from invalid key
+  EXPECT_EQ(
+      state_cache_p->amf_ue_contexts.imsi_amf_ue_id_htbl.remove(5),
+      MAP_KEY_NOT_EXISTS);
+
+  // Object table
+  EXPECT_EQ(
+      state_cache_p->amf_ue_contexts.guti_ue_context_htbl.insert(guti_1, 100),
+      MAP_OK);
+  EXPECT_EQ(
+      state_cache_p->amf_ue_contexts.guti_ue_context_htbl.insert(guti_1, 400),
+      MAP_KEY_ALREADY_EXISTS);
+
+  EXPECT_EQ(
+      state_cache_p->amf_ue_contexts.guti_ue_context_htbl.insert(guti_2, 200),
+      MAP_OK);
+
+  EXPECT_EQ(
+      state_cache_p->amf_ue_contexts.guti_ue_context_htbl.get(
+          guti_1, &gutiData),
+      MAP_OK);
+  EXPECT_EQ(gutiData, 100);
+
+  EXPECT_EQ(
+      state_cache_p->amf_ue_contexts.guti_ue_context_htbl.remove(guti_1),
+      MAP_OK);
+  EXPECT_EQ(
+      state_cache_p->amf_ue_contexts.guti_ue_context_htbl.get(
+          guti_1, &gutiData),
+      MAP_KEY_NOT_EXISTS);
+
+  delete state_cache_p;
 }
 
 }  // namespace magma5g
